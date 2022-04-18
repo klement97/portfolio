361db004315754ac3cd3071134f1110352c0dbe9
Klement Omeri <klementomeri97@gmail.com>
2022-04-18 04:54:20 +0200

    feat: implement first version of history navigation

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const inputEl = document.getElementById('input')
 const terminalEl = document.getElementById('terminal')
 const historyEl = document.getElementById('history')
 const history = []
+let historyIndex = null;
 
 const pushToHistory = (text) => {
     const p = document.createElement('p')
-    p.textContent = text
+    p.textContent = `${path}  ${text}`
     historyEl.appendChild(p)
-    history.push(text)
+    if (text !== '') {
+        history.push(text)
+        historyIndex = history.length - 1
+    }
+    console.log(history, historyIndex)
 }
 
 const removeChildNodes = (parent) => {
@@ -20,7 +25,7 @@ const removeChildNodes = (parent) => {
 const keyDownCallbacks = {
     Enter: () => {
         const value = inputEl.value
-        pushToHistory(`${path}  ${value}`)
+        pushToHistory(value)
         const command = commands[value]
         if (command !== undefined) {
             command()
@@ -28,8 +33,33 @@ const keyDownCallbacks = {
         inputEl.value = null
     },
     ArrowUp: () => {
+        console.log('up', historyIndex)
+        if (historyIndex === null) {
+            return
+        }
+        inputEl.value = history[historyIndex]
+        if (historyIndex > 0) {
+            historyIndex--
+        }
+
+        // After pressing up, the cursor moves to the beginning of the input,
+        // we will move the cursor to the end of the input instead.
+        const end = inputEl.value.length;
+        inputEl.setSelectionRange(end, end);
+        inputEl.focus();
     },
     ArrowDown: () => {
+        console.log('down', historyIndex)
+        if (historyIndex === null) {
+            return
+        }
+        if (historyIndex < history.length - 1) {
+            historyIndex++
+            inputEl.value = history[historyIndex]
+        }
+        else {
+            inputEl.value = ''
+        }
     }
 }
 
